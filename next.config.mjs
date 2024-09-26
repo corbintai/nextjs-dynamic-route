6d674409ffc5cae4e5e4b47c05de400b68965eb6
TaiPT <corbintai@angelpj.com>
2024-09-26 15:01:18 +0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    output: 'export',
-    // Ensure rewrites/redirects aren't breaking dynamic routes
-    async redirects() {
-      return [];
-    },
 };
 
 export default nextConfig;
